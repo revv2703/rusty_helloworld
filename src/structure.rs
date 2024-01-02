4309a79d9876ceb43ef93ef67bb239a47afe673b
revv2703 <asijaprakash27@gmail.com>
2024-01-02 23:49:23 +0530

    explored structures

--- src/structure.rs
+++ src/structure.rs
@@ -8,12 +8,37 @@ struct Rusty{   // Start a struct name with capital
     is_awesome: bool
 }
 
+// You can also create a struct with methods.
 impl Rusty{
     fn boo(){
         println!("Boo!");
     }
     fn check_rust(&self){
         if self.is_rust == true {println!("Is Rust");}  // Need parenthesis
+    }
+}
+
+
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32
+}
+
+impl Rectangle{
+    fn calc(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// Can create multiple implementation blocks for the same struct
+impl Rectangle{
+    fn square(size: u32) -> Rectangle{
+        Rectangle{width: size, height: size}
     }
 }
 
@@ -29,7 +54,23 @@ pub fn main(){
 
     // You can also create a tuple struct, which is a tuple that is a struct.
     struct Color(i32, i32, i32);
-    struct Point(i32, i32, i32);
     let black = Color(0, 0, 0);
+
+    
+    // You can also create a unit struct, which is a struct with no fields.
+
+    let rect = Rectangle{width: 40, height: 50};
+    println!("The area of rectangle: {}", rect.calc());  // "&" because u don't want ownership of the value
+
+
+    let rect1 = Rectangle{
+        width: 10,
+        height: 40
+    };
+    println!("Rectangle can hold rect1: {}", rect.can_hold(&rect1));
+
+
+    let sq = Rectangle::square(10);
+    println!("Square: {:#?}", sq);
  
 }
